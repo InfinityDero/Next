a7778c2fac1a1e1340c3ffa853aab7f67c45f3e7
ron <ron.abramovitz2007@gmail.com>
2026-03-12 11:56:11 +0200

    added the longest name func that returns the longset name in the file

--- one_liners.py
+++ one_liners.py
@@ -1,29 +1,13 @@
-from scapy.all import *
+import functools
 
-FIRST_PACKET = 0
+
+def longest_name(file_path):
+    with open(file_path, 'r') as f:
+        print(functools.reduce(lambda x, y: x if len(x) > len(y) else y, f.readlines(), ''))
 
 
 def main():
-    while(True):
-        packet = sniff()
-        s = packet[FIRST_PACKET]
-        if s[Ether].dst == '08:00:27:9a:ce:39':
-            s[Ether].dst = '0a:00:27:00:00:3A'
-            if ARP in s:
-                s[ARP].pdst = '192.168.240.1'
-                s[ARP].hwsrc = '0a:00:27:00:00:3A'
-            if IP in s:
-                s[IP].dst = '192.168.240.1'
-            sendp(s, iface="enp0s8")
-        else:
-            s[Ether].src = '08:00:27:9a:ce:39'
-            if ARP in s:
-                s[ARP].psrc = '192.168.240.4'
-                s[ARP].hwsrc = '08:00:27:9a:ce:39'
-            if IP in s:
-                s[IP].src = '192.168.240.4'
-            sendp(s, iface="enp0s9")
-
+    longest_name("C:\\bis\\next\\names.txt")
 
 
 if __name__ == "__main__":
