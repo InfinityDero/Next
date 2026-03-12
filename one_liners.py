f80b2a2d1d8ae3afc1e8722808f91bea8fd3022b
ron <ron.abramovitz2007@gmail.com>
2026-03-12 13:42:05 +0200

    added a func that create a new file with the lengths of all names from the original file

--- one_liners.py
+++ one_liners.py
@@ -17,11 +17,18 @@ def shortest_names(file_path):
         print("\n".join([short_name.strip() for short_name in l if len(short_name.strip()) <= len(l[0])]))
 
 
+def create_file_of_names_lengths(file_path, path_of_new_file):
+    with open(file_path, 'r') as f:
+        with open(path_of_new_file, 'w') as f_lengths: 
+            f_lengths.writelines('\n'.join([str(len(name.strip())) for name in f.readlines()]))
+
+
 def main():
     longest_name("C:\\bis\\next\\names.txt")
     file_length("C:\\bis\\next\\names.txt")
     shortest_names("C:\\bis\\next\\names.txt")
-    
+    create_file_of_names_lengths("C:\\bis\\next\\names.txt", "C:\\bis\\next\\names_length.txt")
+
 
 if __name__ == "__main__":
     main()
